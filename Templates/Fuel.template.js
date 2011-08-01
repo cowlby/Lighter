e934ff03610eff274fe28f6e2eefd8cc96d99a8c
Jose Prado <pradador@me.com>
2011-07-31 22:17:28 -0500

    Updated the Fuel and Flame templates for consistency.

--- Templates/Fuel.template.js
+++ Templates/Fuel.template.js
@@ -1,45 +1,45 @@
-/**
- * Script:
- *   Fuel.template.js - Template to build custom Fuel for Lighter.js.
- *
- * License:
- *   MIT-style license.
- * 
- * Author:
- *   Jose Prado
- *
- * Copyright:
- *   Copyright (©) 2009 [Jose Prado](http://pradador.com/).
- *
- */
+/*
+---
+description: Template to build custom Fuel for Lighter.js.
+
+license: MIT-style
+
+authors:
+- Jose Prado
+
+requires:
+  core/1.2.4: '*'
+
+provides: [Fuel.myFuel.js]
+ ...
+*/
 Fuel.myFuel = new Class ({
 	
 	Extends: Fuel,
 	language: '',
 	
-	initialize: function(lighter, options) {
-		
+	initialize: function(options)
+	{
 		/** Set of keywords in CSV form. Add multiple keyword hashes for differentiate keyword sets. */
-		this.keywords = new Hash({
+		this.keywords = {
 			/*keywords: {
 				csv: "keyword1, keyword2, keyword3",
 				alias: 'kw0'
 			},*/
-		}),
+		},
 		
 		/** Set of RegEx patterns to match */
-		this.patterns = new Hash({
+		this.patterns = {
 			//'slashComments': {pattern: this.common.slashComments, alias: 'co0'},
-		});
+		};
 		
 		/** Script Delimiters. Remove or set to null if not applicable. */
-		this.delimiters = new Hash({
+		this.delimiters = {
 			start: this.strictRegExp('startTag1', 'startTag2'),
 			end: this.strictRegExp('endTag1')
-		});
+		};
 		
 		// Call parent constructor AFTER instance variables are set.
-		this.parent(lighter, options);
+		this.parent(options);
 	}
-	
 });
