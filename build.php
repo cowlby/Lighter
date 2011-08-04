025513abfa1ad942173ebdd9a7ec666ce496c4fb
Jose Prado <pradador@me.com>
2011-08-03 19:09:14 -0500

    Completed a basic PHP build script.

--- build.php
+++ build.php
@@ -1,36 +1,67 @@
+<pre>
 <?php
 
-require_once 'Vendor/Packager/packager.php';
-require_once 'Vendor/ClassLoader/UniversalClassLoader';
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Symfony\Component\Finder\Finder;
 
-$loader = new UniversalClassLoader();
-
-// register classes with namespaces
-$loader->registerNamespaces(array(
-    'Symfony\Component' => __DIR__.'/component',
-    'Symfony' => __DIR__.'/framework',
-    'Sensio' => array(__DIR__.'/src', __DIR__.'/vendor'),
-));
-
-$loader->register();
+require_once __DIR__ . '/Vendor/Packager/packager.php';
+require_once __DIR__ . '/Vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $packageDir = __DIR__;
 $buildDir   = __DIR__ . '/Build';
-$pkg = new Packager($packageDir);
-
-echo "<pre>";
-foreach ($pkg->get_all_files() as $file) {
-    echo $file . "\n";
-    echo 'Provides:    ' . join(',', $pkg->get_file_provides($file)) . "\n";
-    echo 'Depends on:  ' . join(', ', $pkg->get_file_dependancies($file)) . "\n";
-    echo 'Lives in     ' . $pkg->get_file_path($file) . "\n";
-    echo 'Description: ' . $pkg->get_file_description($file) . "\n";
-    echo "\n";
-}
+$vendorDir  = __DIR__ . '/Vendor';
+$sourceDir  = __DIR__ . '/Source';
+$stylesDir  = __DIR__ . '/Styles';
 
 if (!is_dir($buildDir)) {
     mkdir($buildDir);
 }
 
-$pkg->write_from_files($buildDir . '/Lighter.js', $pkg->get_all_files());
+if (!is_dir($buildDir . '/Flames')) {
+    mkdir($buildDir . '/Flames');
+}
 
+if (!is_dir($buildDir . '/Fuels')) {
+    mkdir($buildDir . '/Fuels');
+}
+
+// Packager
+$pkg = new Packager($packageDir);
+
+echo "Building Lighter.js\n";
+foreach ($pkg->get_all_files() as $file) {
+    printf("* %s \n", $file);
+}
+$pkg->write_from_files($buildDir . '/Lighter.js', $pkg->get_all_files());
+echo "\n";
+
+// Custom Build
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array('Symfony\Component' => $vendorDir));
+$loader->register();
+
+$fuels = Finder::create()
+    ->files()
+    ->name('/^Fuel\.(\w+)\.js$/')
+    ->in($sourceDir);
+
+echo "Copying Fuels\n";
+foreach ($fuels as $fuel) {
+    copy($fuel->getRealPath(), $buildDir . '/Fuels/' . $fuel->getFilename());
+    printf("* %s \n", $fuel->getFilename());
+}
+echo "\n";
+
+$flames = Finder::create()
+    ->files()
+    ->name('/^Flame\.(\w+)\.css$/')
+    ->in($stylesDir);
+    
+echo "Copying Flames\n";
+foreach ($flames as $flame) {
+    copy($flame->getRealPath(), $buildDir . '/Flames/' . $flame->getFilename());
+    printf("* %s \n", $flame->getFilename());
+}
+echo "\n";
+?>
+</pre>
